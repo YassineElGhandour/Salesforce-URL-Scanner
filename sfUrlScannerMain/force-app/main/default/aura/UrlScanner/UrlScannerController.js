f43c7195bf550d77cb2f1562185b4a344e6b54ea
Yassine El Ghandour <yassine.elghandour@flowhaven.com>
2022-06-03 14:07:25 +0300

    feat: add a scanner from ip quality ep and status in datatable

--- sfUrlScannerMain/force-app/main/default/aura/UrlScanner/UrlScannerController.js
+++ sfUrlScannerMain/force-app/main/default/aura/UrlScanner/UrlScannerController.js
@@ -2,7 +2,8 @@
     init: function (cmp, event, helper) {
         // Set one column for the aura databable which is URL
         cmp.set('v.columns', [
-            { label: 'URL', fieldName: 'url', type: 'text'}
+            { label: 'URL', fieldName: 'url', type: 'text'},
+            { label: 'Status', fieldName : 'status', type: 'text'}
         ]);
     },
     
