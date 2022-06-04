edf6809ce6bf696761c0509e9d60acf78f49d354
Yassine El Ghandour <yassine.elghandour@flowhaven.com>
2022-06-04 13:37:00 +0300

    feat: add scanner for malicious urls and unit tests

--- sfUrlScannerMain/force-app/main/default/aura/UrlScanner/UrlScannerHelper.js
+++ sfUrlScannerMain/force-app/main/default/aura/UrlScanner/UrlScannerHelper.js
@@ -1,7 +1,7 @@
 ({
 	scanUrlsInFileInHelper : function(cmp, event) {
         // call action from the APEX controller UrlScannerController
-        var action = cmp.get("c.scanUrlsInFile");
+        var action = cmp.get("c.fetchUrlsAndTheirStatuses");
         var caseId = cmp.get("v.recordId");
         
         action.setParams({
@@ -15,21 +15,42 @@
             
             if(state === "SUCCESS") {
                 var data = response.getReturnValue();
+                
                 // Populate data from the response of UrlScannerController APEX, which is a map where the key is 'url' and the results are the urls of said method
-                cmp.set("v.data", data);
+                cmp.set("v.data", data.list);
                 // showList by default is false, and is only set as true when the state of this action is success to show the datatable only after attaching a file to the case
                 cmp.set("v.showList", true);
                 
-                toastEvent.setParams({
-                    "type": "success",
-                    "title" : "Success",
-                    "message" : "File is attached to the case succesfully"
-                });
+                if(!data.status) {
+                    toastEvent.setParams({
+                        "type": "success",
+                        "title" : "Success",
+                        "message" : "File is attached to the case succesfully"
+                    });
+                }
+                else {
+                    toastEvent.setParams({
+                        "type": "error",
+                        "title" : "Malicious file",
+                        "message" : "File attached contains malicious urls and will be deleted from this case"
+                    });
+                }
                 toastEvent.fire();
-                
             }
-            else {
-                console.log('error');
+        });
+        $A.enqueueAction(action);
+	},
+    
+    showToastMessage : function(cmp, event) {
+        var action = cmp.get("c.getIfFileIsMalicious");
+        
+        action.setCallback(this, function(response){
+            // Init toast to display success message
+            var toastEvent = $A.get("e.force:showToast");
+            var state = response.getState();
+            
+            if(state === "SUCCESS") {
+                
             }
         });
         $A.enqueueAction(action);
